7e320d6bd828e661da9af11f3ccdaf5689db6c5b
Mateus Esteves <mateus.esteves1994@gmail.com>
2018-04-15 10:37:45 -0300

    Modificações no TimelineService

--- app/services/timeline.service.js
+++ app/services/timeline.service.js
@@ -2,13 +2,27 @@
     'use strict';
     
     angular.module('nwjs-angular')
-    .service('TimelineService', function TimelineService($http) {
-       
+    .service('TimelineService', function TimelineService($http, $q) {
+        
         this.fetchTimelineData = function () {
-            return $http({
+            let deferred = $q.defer();
+            
+            $http({
                 method: 'GET',
                 url: 'https://private-7cf60-4youseesocialtest.apiary-mock.com/timeline'
+            }).then(function onSuccess(response) {
+                deferred.resolve({
+                    error: false,
+                    data: response.data
+                });
+            }, function onError(error) {
+                deferred.resolve({
+                    error: true,
+                    data: error
+                });
             });
+            
+            return deferred.promise;
         }
         
     });
