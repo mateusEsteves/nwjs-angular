c40f15b26c4920081571de9889a0c1860179227c
Mateus Esteves <mateus.esteves1994@gmail.com>
2018-04-15 16:53:34 -0300

    Adicionado barra de titulo e sidenav com os filtros

--- app/pages/search/search.controller.js
+++ app/pages/search/search.controller.js
@@ -2,11 +2,12 @@
     'use strict';
     
     angular.module('nwjs-angular')
-    .controller('SearchController', function SearchController($scope, $filter, TimelineService) {
+    .controller('SearchController', function SearchController($scope, $filter, $mdSidenav, TimelineService) {
         
         $scope.timelineDataOriginal = [];
         $scope.timelineDataToShowOnPage = [];
         $scope.filters = {};
+        
         
         function initializeController() {
             TimelineService.fetchTimelineData()
@@ -19,22 +20,33 @@
         }
         
         initializeController();
-
+        
         function trimUnfilledFilters(filterObject) {
             let trimmedFilter = {};
-
+            
             Object.keys(filterObject).forEach((key) => {
                 if (filterObject[key].toString() !== '') {
                     trimmedFilter[key] = filterObject[key]
                 }
             });
-
+            
             return trimmedFilter;
         }
-
+        
         $scope.filterData = function () {
             let filter = trimUnfilledFilters($scope.filters);
             $scope.timelineDataToShowOnPage = $filter('timelineDataFilter')($scope.timelineDataOriginal, filter);
+            $scope.toggleSidebar();
         }
+        
+        $scope.clearFilters = function () {
+            $scope.timelineDataToShowOnPage = angular.copy($scope.timelineDataOriginal);
+            $scope.toggleSidebar();
+        }
+        
+        $scope.toggleSidebar = function () {
+            $mdSidenav('right').toggle();
+        }
+        
     });
 })();
